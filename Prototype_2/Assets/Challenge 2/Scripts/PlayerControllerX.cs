2daf76a7b127595d2b292a53fcd1f28d29392298
skylor <skylorvd@gmail.com>
2021-02-12 08:45:34 +0100

    Change: added the 2de one
    Type: addition Why: needed for school

---
+++ Prototype_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spamLockinterval = 1.0f;
+    private bool locked;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&&!locked)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            locked = true;
+            Invoke("Unlock", spamLockinterval);
+        }
+    }
+    void Unlock()
+    {
+        locked = false;
+    }
+}
